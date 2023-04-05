6ba7a6843b07a058583b7a0d42b5d38310f981d5
enricosebastian <enricosebastiansalazar@gmail.com>
2023-04-05 18:49:12 +0800

    checing scss files

--- components/ArticleCards/BannerArticle.js
+++ components/ArticleCards/BannerArticle.js
@@ -5,17 +5,15 @@ import cleanArticleExcerpt from "../Functions/cleanArticleExcerpt";
 
 export default function BannerArticle({article}) {
     const title = article.title["rendered"].replace(/<\/?[^>]+(>|$)/g, ""); // Solution by https://stackoverflow.com/questions/5002111/how-to-strip-html-tags-from-string-in-javascript
-    // const authorsList = createAuthorsList(article.authors);
-
+    const authorsList = createAuthorsList(article.authors);
     const excerpt = cleanArticleExcerpt(article.excerpt["rendered"]);
-    console.log(excerpt);
 
     return (
         <a className={styles.article__wrapper__full} href={`/presents/${article.slug}`}>
             <img className={styles.article__image__full} src={article.jetpack_featured_media_url}/>
             <div className={styles.article__information__wrapper}>
                 <div className={styles.article__headline__wrapper}>{title}</div>
-                <div className={styles.article__authors__wrapper}>by authorsList</div>
+                <div className={styles.article__authors__wrapper}>by {authorsList}</div>
                 <div className={styles.article__snippet__wrapper}>
                     {excerpt}
                     <span className={styles.snippet__readMore__link}>Read More</span>
--- components/Functions/createAuthorsList.js
+++ components/Functions/createAuthorsList.js
@@ -1,24 +1,71 @@
-export default function createAuthorsList(authors){
+export default function createAuthorsList(authors, options="none"){
+    
+    if(options === "none") {
+        return createAuthorListWithNoLink(authors);
+    } else {
+        return createAuthorListWithLink(authors);
+    }
+}
+
+function createAuthorListWithNoLink(authors) {
     let concatonatedAuthors = [];
+
     for(let i=0; i<authors.length; i++) {
         if(authors.length > 2) {
             if(i == 0) {
-                concatonatedAuthors.push(<a key={authors[i].display_name} href={`/by/${authors[i].slug}`}>{authors[i].display_name}</a>);
-                concatonatedAuthors.push(<span>{", "}</span>);
+                concatonatedAuthors.push(<span key={`${authors[i].display_name}-comma`}>{`${authors[i].display_name}, `}</span>);
             } else {
-                concatonatedAuthors.push(<span>{"and "}</span>);
-                concatonatedAuthors.push(<a key={authors[i].display_name} href={`/by/${authors[i].slug}`}>{authors[i].display_name}</a>);
+                concatonatedAuthors.push(<span key={`${authors[i].display_name}-and`}>{`and ${authors[i].display_name}`}</span>);
             }
         } else if(authors.length == 2) {
             if(i == 0) {
-                concatonatedAuthors.push(<a key={authors[i].display_name} href={`/by/${authors[i].slug}`}>{authors[i].display_name}</a>);
+                concatonatedAuthors.push(<span key={`${authors[i].display_name}-comma`}>{`${authors[i].display_name}`}</span>);
             } else {
-                concatonatedAuthors.push(<span>{" and "}</span>);
-                concatonatedAuthors.push(<a key={authors[i].display_name} href={`/by/${authors[i].slug}`}>{authors[i].display_name}</a>);
+                concatonatedAuthors.push(<span key={`${authors[i].display_name}-comma`}>{` and ${authors[i].display_name}`}</span>);
             }
         } else {
-            concatonatedAuthors.push(<a key={authors[i].display_name} href={`/by/${authors[i].slug}`}>{authors[i].display_name}</a>);
+            concatonatedAuthors.push(<span key={`${authors[i].display_name}-comma`}>{authors[i].display_name}</span>);
         }
     }
-    return (concatonatedAuthors);
+    return concatonatedAuthors;
 }
+
+function createAuthorListWithLink(authors) {
+    let concatonatedAuthors = [];
+
+    for(let i=0; i<authors.length; i++) {
+        if(authors.length > 2) {
+            if(i == 0) {
+                concatonatedAuthors.push(
+                    <a 
+                        key={`${authors[i].display_name}-link`} 
+                        href={`/by/${authors[i].slug}`}
+                    >
+                        {authors[i].display_name}
+                    </a>
+                );
+                concatonatedAuthors.push(
+                    <span 
+                        key={`${authors[i].display_name}-comma`}
+                    >
+                        {", "}
+                    </span>
+                );
+            } else {
+                concatonatedAuthors.push(<span key={`${authors[i].display_name}-and`}>{"and "}</span>);
+                concatonatedAuthors.push(<a key={`${authors[i].display_name}-link`} href={`/by/${authors[i].slug}`}>{authors[i].display_name}</a>);
+            }
+        } else if(authors.length == 2) {
+            if(i == 0) {
+                concatonatedAuthors.push(<a key={`${authors[i].display_name}-link`} href={`/by/${authors[i].slug}`}>{authors[i].display_name}</a>);
+            } else {
+                concatonatedAuthors.push(<span key={`${authors[i].display_name}-and`}>{" and "}</span>);
+                concatonatedAuthors.push(<a key={`${authors[i].display_name}-link`} href={`/by/${authors[i].slug}`}>{authors[i].display_name}</a>);
+            }
+        } else {
+            concatonatedAuthors.push(<a key={`${authors[i].display_name}-link`} href={`/by/${authors[i].slug}`}>{authors[i].display_name}</a>);
+        }
+    }
+    return concatonatedAuthors;
+}
+
