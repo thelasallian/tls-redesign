a687ed20cd032cc371d62dd86de95fc1cfe9f878
enricosebastian <enricosebastiansalazar@gmail.com>
2023-04-06 17:26:14 +0800

    restructured folder paths

--- components/ArticleCards/BannerArticleMobile.js
+++ components/ArticleCards/BannerArticleMobile.js
@@ -1,10 +1,22 @@
 import React from "react";
+import styles from "@/styles/BannerArticle/BannerArticleMobile.module.scss";
+import createAuthorsList from "../Functions/createAuthorsList";
+import cleanArticleExcerpt from "../Functions/cleanArticleExcerpt";
 
-export default function BannerArticleMobile() {
+
+export default function BannerArticleFull({article}) {
+    const headline = article.title["rendered"].replace(/<\/?[^>]+(>|$)/g, ""); // Solution by https://stackoverflow.com/questions/5002111/how-to-strip-html-tags-from-string-in-javascript
+    const authors = createAuthorsList(article.authors);
+    const excerpt = cleanArticleExcerpt(article.excerpt["rendered"]);
 
     return (
-        <div>
-            banner article mobile
-        </div>
+        <a className={styles.article__link__banner} href={`/presents/${article.slug}`}>
+
+            <div className={styles.article__wrapper__banner}>
+                <img className={styles.article__image__img} src={article.jetpack_featured_media_url}/>
+            </div>
+
+            
+        </a>
     );
 }
--- components/body/index/Body.js
+++ components/body/index/Body.js
@@ -1,8 +1,64 @@
-import React from "react";
-import BodyFull from "./BodyFull";
+import React, { useEffect, useState } from "react";
+import styles from "@/styles/Body/index/Body.module.scss";
 
-export default function Body({sections}) {
+import BannerArticleFull from "@/components/ArticleCards/BannerArticleFull";
+import BannerArticleMobile from "@/components/ArticleCards/BannerArticleMobile";
+
+// Swiper library
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Pagination, Navigation } from "swiper";
+
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+export default function BodyFull({sections}) {
+    
+    const [windowWidth, setWindowWidth] = useState(0);
+
+    const handlingWindowResize = () => {
+        setWindowWidth(window.innerWidth);
+    }
+
+    const bannerArticles = sections.map(section => section.articles[0]);
+    const bannerArticleCards = bannerArticles.map(bannerArticle =>
+        (windowWidth < 750) ? 
+        <SwiperSlide><BannerArticleMobile key={bannerArticle.id} article={bannerArticle}/></SwiperSlide>:
+        <SwiperSlide><BannerArticleFull key={bannerArticle.id} article={bannerArticle}/></SwiperSlide>
+    );
+
+    //Single-responsibility useEffects
+    useEffect(() => {
+        setWindowWidth(window.innerWidth);
+        window.addEventListener("resize", handlingWindowResize);
+    },[]);
+
     return (
-        <BodyFull sections={sections}/>
+        <div className={styles.body__wrapper__full}>
+            <div className={styles.section__banners__full}>
+                <Swiper
+                    spaceBetween={30}
+                    loop={true}
+                    autoplay={{
+                      delay: 2500,
+                      disableOnInteraction: false,
+                    }}
+                    modules={[Autoplay]}
+                    className="mySwiper"
+                >
+                    {bannerArticleCards}
+                </Swiper>
+            </div>
+
+            <div className={styles.section__articles__full}>
+                lorem
+            </div>
+            <div className={styles.section__articles__full}>
+                lorem
+            </div>
+            <div className={styles.section__articles__full}>
+                lorem
+            </div>
+        </div>
     );
 }
