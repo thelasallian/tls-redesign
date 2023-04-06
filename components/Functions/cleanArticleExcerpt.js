08c3dd801fd3042d38ff81d5a5508d1980a96a4b
enricosebastian <enricosebastiansalazar@gmail.com>
2023-04-06 08:42:02 +0800

    added word iteration index finder

--- components/Functions/cleanArticleExcerpt.js
+++ components/Functions/cleanArticleExcerpt.js
@@ -2,5 +2,13 @@ export default function cleanArticleExcerpt(excerpt) {
     excerpt = excerpt.replace(/<[^>]+>/g, '')
     let ellipsisIndex = excerpt.indexOf("&hellip;");
 
+    // let finalWordIndex = getPositionOfString(excerpt, " ", 50);
+
     return excerpt.substring(0, ellipsisIndex)+"...";
+}
+
+
+// Reference: https://stackoverflow.com/questions/14480345/how-to-get-the-nth-occurrence-in-a-string
+function getPositionOfString(entry, targetString, index=0) {
+  return entry.split(targetString, index).join(targetString).length;
 }
