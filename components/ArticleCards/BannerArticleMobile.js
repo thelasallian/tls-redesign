64ff21eb0714e50124aecc0606d4e7972751e7d4
enricosebastian <enricosebastiansalazar@gmail.com>
2023-04-07 11:33:35 +0800

    changed styling for banner articles in mobile

--- components/ArticleCards/BannerArticleMobile.js
+++ components/ArticleCards/BannerArticleMobile.js
@@ -8,9 +8,30 @@ import getNthWord from "../Functions/getNthWord";
 export default function BannerArticleFull({article}) {
     let headline = article.title["rendered"].replace(/<\/?[^>]+(>|$)/g, ""); // Solution by https://stackoverflow.com/questions/5002111/how-to-strip-html-tags-from-string-in-javascript
     const authors = createAuthorsList(article.authors);
-    const excerpt = cleanArticleExcerpt(article.excerpt["rendered"], 25);
-    headline = getNthWord(headline, 20);
+    let excerpt = cleanArticleExcerpt(article.excerpt["rendered"]);
+    excerpt = getNthWord(excerpt, 15);
+    // headline = getNthWord(headline, 10);
 
+    return (
+        <a className={styles.article__link__banner} href={`/presents/${article.slug}`}>
+
+            <div className={styles.article__image__banner}>
+                <img className={styles.article__image__img} src={article.jetpack_featured_media_url}/>
+            </div>
+
+            <div className={styles.article__information__wrapper}>
+                <div className={styles.article__headline__wrapper}>{headline}</div>
+                <div className={styles.article__author__wrapper}>by {authors}</div>
+            </div>
+
+
+        </a>
+    );
+}
+
+
+// deprecated
+function BannerArticleWithExcerpt() {
     return (
         <a className={styles.article__link__banner} href={`/presents/${article.slug}`}>
 
