ab2ee413c3ba970b0f725747fcbb89d2b4e2966a
enricosebastian <enricosebastiansalazar@gmail.com>
2023-04-08 09:01:05 +0800

    added initial styling for primary section

--- components/ArticleCards/Style1/PrimaryArticle.js
+++ components/ArticleCards/Style1/PrimaryArticle.js
@@ -1,15 +1,22 @@
 import React from "react";
 import styles from "@/styles/ArticleCards/Style1/PrimaryArticle.module.scss";
+import createAuthorsList from "../../Functions/createAuthorsList";
+import cleanArticleExcerpt from "../../Functions/cleanArticleExcerpt";
+
 
 export default function PrimaryArticle({article}) {
+    const headline = article.title["rendered"].replace(/<\/?[^>]+(>|$)/g, ""); // Solution by https://stackoverflow.com/questions/5002111/how-to-strip-html-tags-from-string-in-javascript
+    const authors = createAuthorsList(article.authors);
+    const excerpt = cleanArticleExcerpt(article.excerpt["rendered"]);
+
     return (
         <a className={styles.articles__primary__wrapper} href={`/presents/${article.slug}`}>
             <div className={styles.article__image__wrapper}>
                 <img className={styles.article__image__img} src={article.jetpack_featured_media_url}/>
             </div>
             <div className={styles.article__information__wrapper}>
-                <div className={styles.article__headline__wrapper}>headline</div>
-                <div className={styles.article__author__wrapper}>by authors</div>
+                <div className={styles.article__headline__wrapper}>{headline}</div>
+                <div className={styles.article__author__wrapper}>by {authors}</div>
             </div>
         </a>
     );
