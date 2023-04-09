4c49e3932db0b638b216815f4c00600a9fe11103
enricosebastian <enricosebastiansalazar@gmail.com>
2023-04-09 16:23:01 +0800

    working secondary articles

--- components/Section/Style1/Section.js
+++ components/Section/Style1/Section.js
@@ -1,15 +1,21 @@
 import React from "react";
 import styles from "@/styles/Section/Style1/Section.module.scss";
 import PrimaryArticle from "@/components/ArticleCards/Style1/PrimaryArticle";
+import SecondaryArticle from "@/components/ArticleCards/Style1/SecondaryArticle";
 
 export default function Section({section}) {
+    const secondaryArticles = section.articles.slice(2);
+    const secondaryArticlesSection = secondaryArticles.map(secondaryArticle => <SecondaryArticle article={secondaryArticle}/>);
+
     return (
         <div className={styles.section__area__wrapper}>
-            <div className={styles.section__headline__wrapper}>{section.name}</div>
+            <div className={styles.section__title__wrapper}>
+                <a className={styles.section__title__link} href={`/section/${section.name.toLowerCase()}`}>{section.name}</a>
+            </div>
             <div className={styles.section__articles__wrapper}>
                 <PrimaryArticle article={section.articles[1]}/>
                 <div className={styles.articles__secondary__wrapper}>
-                    secondary wrapper
+                    {secondaryArticlesSection}
                 </div>
             </div>
         </div>
