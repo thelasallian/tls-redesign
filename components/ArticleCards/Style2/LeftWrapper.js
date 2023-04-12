e8953b10526332270550397255718b282074a68e
enricosebastian <enricosebastiansalazar@gmail.com>
2023-04-13 02:49:16 +0800

    finalized primary article left wrapper module

--- components/ArticleCards/Style2/LeftWrapper.js
+++ components/ArticleCards/Style2/LeftWrapper.js
@@ -4,33 +4,39 @@ import createAuthorsList from "../../Functions/createAuthorsList";
 import cleanArticleExcerpt from "../../Functions/cleanArticleExcerpt";
 
 
-export default function PrimaryArticle({articles}) {
+export default function LeftWrapper({articles}) {
+
+    const secondaryArticles = articles.slice(1).map(article => 
+        <a className={styles.article__link__wrapper} href="">
+            <div className={styles.articles__secondary__wrapper}>
+                <div className={styles.article__headline__wrapper}>
+                    {article.title["rendered"]}
+                </div>
+                <div className={styles.article__author__wrapper}>
+                    by {createAuthorsList(article.authors)}
+                </div>
+            </div>
+        </a>
+    );
+
+    console.log(secondaryArticles);
 
     return (
         <div className={styles.articles__wrapper__left}>
-            <div className={styles.articles__primary__wrapper}>
-                <div className={styles.article__headline__wrapper}>
-                    {articles[0].title["rendered"]}
+            <a className={styles.article__link__wrapper} href={`/presents/${articles[0].slug}`}>
+                <div className={styles.articles__primary__wrapper}>
+                    <div className={styles.article__headline__wrapper}>
+                        {articles[0].title["rendered"]}
+                    </div>
+                    <div className={styles.article__author__wrapper}>
+                        by {createAuthorsList(articles[0].authors)}
+                    </div>
+                    <div className={styles.article__snippet__wrapper} dangerouslySetInnerHTML={{__html: articles[0].excerpt["rendered"]}}>
+                    </div>
                 </div>
-                <div className={styles.article__author__wrapper}>
-                    by author
-                </div>
-                <div className={styles.article__snippet__wrapper}>
-                    The quick brown fox jumps over the lazy dog.
-                </div>
-            </div>
-            <div className={styles.articles__secondary__wrapper}>
-                aritlcle 2
-            </div>
-            <div className={styles.articles__secondary__wrapper}>
-                aritlcle 3
-            </div>
-            <div className={styles.articles__secondary__wrapper}>
-                aritlcle 4
-            </div>
-            <div className={styles.articles__secondary__wrapper}>
-                aritlcle 5
-            </div>
+            </a>
+
+            {secondaryArticles}
         </div>
     );
 }
