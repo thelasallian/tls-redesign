6ef8577f7d5397111050f7e0d6f6cd1376a5074f
enricosebastian <enricosebastiansalazar@gmail.com>
2023-04-21 15:17:22 +0800

    fixed additional html codes for utc

--- components/ArticleCards/Style1/PrimaryArticle.js
+++ components/ArticleCards/Style1/PrimaryArticle.js
@@ -2,10 +2,10 @@ import React from "react";
 import styles from "@/styles/ArticleCards/Style1/PrimaryArticle.module.scss";
 import createAuthorsList from "../../Functions/createAuthorsList";
 import cleanArticleExcerpt from "../../Functions/cleanArticleExcerpt";
-
+import dehtml from "@/components/Functions/dehtml";
 
 export default function PrimaryArticle({article}) {
-    const headline = article.title["rendered"].replace(/<\/?[^>]+(>|$)/g, ""); // Solution by https://stackoverflow.com/questions/5002111/how-to-strip-html-tags-from-string-in-javascript
+    const headline = dehtml(article.title["rendered"]);
     const authors = createAuthorsList(article.authors);
     const excerpt = cleanArticleExcerpt(article.excerpt["rendered"]);
 
