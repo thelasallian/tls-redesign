e7f97a8ffba41038ca69612eb1ba342e25284e5a
enricosebastian <enricosebastiansalazar@gmail.com>
2023-05-01 14:17:33 +0800

    added more codes to dehtml function

--- components/Functions/dehtml.js
+++ components/Functions/dehtml.js
@@ -1,13 +1,14 @@
 export default function dehtml(string) {
     string = string
         .replace(/<\/?[^>]+(>|$)/g, "")
-        .replaceAll("&#8217;", "'") // apostrophe
+        .replaceAll("&#8217;", "’") // apostrophe
         .replaceAll("&#39;", "'") // right apostrophe to apostrophe
         .replaceAll("&#8212;", "—") // different dash to em dash
         .replaceAll("&#8211;", "—") // different dash to em dash
         .replaceAll("&#8208;", "-") // normal dash 
         .replaceAll("&#8230;", "...") // ellipsis
         .replaceAll("&hellip;", "...") //elipsis
+        .replaceAll("&#8216;", "‘") // left apostrophe
         .replaceAll("&nbsp;", " "); // non breaking space
     
     return string;
