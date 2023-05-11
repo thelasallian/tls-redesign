9c9beeaee115293df12e3d598629ec427358c60a
enricosebastian <enricosebastiansalazar@gmail.com>
2023-05-11 14:02:03 +0800

    fixed merge conflicts

--- components/Body/article/Body.js
+++ components/Body/article/Body.js
@@ -1,11 +1,6 @@
-import {React, useEffect} from "react";
-import styles from "@/styles/Body/article/Body.module.scss";
-import ArticleCard from "@/components/ArticleCards/card__out/ArticleCard";
-
-import createAuthorsList from "@/components/Functions/createAuthorsList";
-import parseRelatedArticles from "@/components/Functions/parseRelatedArticles";
-import dehtml from "@/components/Functions/dehtml";
-import dayjs from "dayjs";
+import {React, useEffect, useState} from "react";
+import {default as BodyMobile} from "@/components/Body/article/Mobile/Body";
+import {default as BodyFull} from "@/components/Body/article/Full/Body";
 
 export default function Body({article, section}) {
     const [isMobile, setIsMobile] = useState(false);
