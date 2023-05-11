700111f072fca8998c0f6afb32b7fc0a1af49656
enricosebastian <enricosebastiansalazar@gmail.com>
2023-05-11 14:48:39 +0800

    initialized body for section

--- pages/section/[sectionName].js
+++ pages/section/[sectionName].js
@@ -1,7 +1,33 @@
 import { useRouter } from "next/router";
+import Head from "next/head";
+import Header from '@/components/HeaderV2/Header';
+import Body from '@/components/Body/section/Body';
+import Footer from '@/components/Footer/Full/Footer';
 
-export default function SectionPage() {
+export default function SectionPage({sectionName}) {
+    const sectionNameCapitalized = sectionName.charAt(0).toUpperCase() + sectionName.slice(1);
+
     return (
-        <h1>Section with Articles</h1>
+        <>
+            <Head>
+            <title>{sectionNameCapitalized}</title>
+            </Head>
+            <div className="wrapper">
+                <Header/>
+                <Body/>
+                <Footer/>
+            </div>
+        </>
+        
     );
+}
+
+export async function getServerSideProps({params}) {
+    const sectionName = params.sectionName;
+
+    return {
+        props: {
+            sectionName: sectionName
+        },
+    };
 }
