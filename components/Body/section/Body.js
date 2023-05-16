ed22914dc04ff6520db143a12cbb2754db2cd24e
enricosebastian <enricosebastiansalazar@gmail.com>
2023-05-16 14:32:22 +0800

    integrated infinite scrolling

--- components/Body/section/Body.js
+++ components/Body/section/Body.js
@@ -6,6 +6,7 @@ export default function Body({articles, category}) {
 
     const [pageNumber, setPageNumber] = useState(2);
     const [articleData, setArticleData] = useState(articles);
+    const [isFetchingData, setIsFetchingData] = useState(false);
 
     const articleCards = articleData.map(article => 
         <div className={styles.article__card__wrapper} key={`article__card__wrapper-${article.id}`}>
@@ -18,23 +19,42 @@ export default function Body({articles, category}) {
             />
         </div>
     );
+    
+    const logCurrentYValue = async () => {
+        const yOffset = 491;
+        const bodyWrapper = document.querySelector("."+styles.body__wrapper__full);
 
-    const fetchItems = async () => {
-        const response = await fetch(`https://thelasallian.com/wp-json/wp/v2/posts?_fields=id,authors,excerpt,title,slug,categories,jetpack_featured_media_url&per_page=20&categories=${category}&page=${pageNumber}`);
-        const newData = await response.json();
-        setArticleData(prevData => prevData.concat(newData));
+        if(bodyWrapper === null) return;
+
+        const bodyWrapperFullHeight = bodyWrapper.offsetHeight - yOffset;
+        const currentYValue = window.pageYOffset;
+
+        if(bodyWrapperFullHeight <= currentYValue) {
+            if(isFetchingData) {
+                console.log("Still fetching data. Please wait!");
+            } else {
+                setIsFetchingData(true);
+                const response = await fetch(`https://thelasallian.com/wp-json/wp/v2/posts?_fields=id,authors,excerpt,title,slug,categories,jetpack_featured_media_url&per_page=20&categories=${category}&page=${pageNumber}`);
+                const newData = await response.json();
+                setArticleData(prevData => prevData.concat(newData));
+            }
+        }
     }
 
-    useEffect(()=> {
-        setPageNumber(prevState => prevState+1);
+    useEffect(() => {
+        setIsFetchingData(false);
+        setPageNumber(prevState => prevState + 1);
     }, [articleData]);
+
+    useEffect(() => {
+        window.addEventListener("scroll", logCurrentYValue);
+    },[]);
 
     return (
         <div className={styles.body__wrapper__full}>
             <div className={styles.body__articles__wrapper}>
                 {articleCards}
             </div>
-            <button onClick={fetchItems}>click</button>
         </div>
     );
 }
