32ca7279318e8eb9b5e3c90d5450b5c898a8f60b
enricosebastian <enricosebastiansalazar@gmail.com>
2023-05-18 10:16:21 +0800

    initialized author pages

--- pages/by/[authorSlug].js
+++ pages/by/[authorSlug].js
@@ -1,7 +1,33 @@
 import { useRouter } from "next/router";
+import Head from "next/head";
 
-export default function AuthorPage() {
+export default function AuthorPage({userData}) {
+    console.log(userData);
+
     return (
-        <h1>Details about an author</h1>
+        <>
+            <Head>
+                <title>{`${userData.name} - The LaSallian`}</title>
+            </Head>
+            <h1>Details about {userData.name}</h1>
+        </>
     );
+}
+
+export async function getServerSideProps({req, res, params}) {
+    const authorSlug = params.authorSlug;
+
+    res.setHeader(
+        'Cache-Control',
+        'public, s-maxage=1, stale-while-revalidate=59'
+    )
+
+    const userResponse = await fetch(`https://thelasallian.com/wp-json/wp/v2/users/?slug=${authorSlug}`);
+    const userData = await userResponse.json();
+
+    return {
+        props: {
+            userData: userData[0],
+        },
+    };
 }
