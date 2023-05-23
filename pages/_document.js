5799aa2a89305ebf502e548f236b8a37b0f1ad98
enricosebastian <enricosebastiansalazar@gmail.com>
2023-05-23 16:52:49 +0800

    added a meta tag for phones with notches

--- pages/_document.js
+++ pages/_document.js
@@ -3,6 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover"/> 
       <Head />
       <body>
         <Main />
