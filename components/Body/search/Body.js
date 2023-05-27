b25df4a8503f263884800a227a8a2d838393cc53
enricosebastian <enricosebastiansalazar@gmail.com>
2023-05-27 09:24:32 +0800

    added more fetching

--- components/Body/search/Body.js
+++ components/Body/search/Body.js
@@ -8,6 +8,7 @@ export default function Body({authors, articles, searchInput, rawSearchInput}) {
     const [articleData, setArticleData] = useState(articles);
     const [isFetching, setIsFetching] = useState(false);
     const [isMobile, setIsMobile] = useState(false);
+    const [hasMoreArticles, setHasMoreArticles] = useState(true);
 
     const authorCards = authors.map(author => 
         <div className={styles.articles__card__wrapper}>
@@ -15,7 +16,7 @@ export default function Body({authors, articles, searchInput, rawSearchInput}) {
         </div>
     );
 
-    const articleCards = articles.map(article => 
+    const articleCards = articleData.map(article => 
         <div className={styles.articles__card__wrapper} key={`articles__card__wrapper-${article.id}`}>
             <ArticleCard 
                 key={`${article.id}-articleCard`}
@@ -30,11 +31,56 @@ export default function Body({authors, articles, searchInput, rawSearchInput}) {
         </div> 
     );
 
+    const fetchArticles = async () => {
+        setIsFetching(true);
+
+        const response = await fetch(`https://thelasallian.com/wp-json/wp/v2/posts?search=${rawSearchInput}&per_page=10&page=${pageNumber}&_fields=id,authors,excerpt,title,slug,categories,jetpack_featured_media_url`);
+        const newData = await response.json();
+
+        console.log("raw: "+rawSearchInput);
+        console.log("pageNumber: "+pageNumber);
+        console.log(newData);
+
+        if(newData.length === undefined) {
+            console.log("No more articles left.");
+            setIsFetching(false);
+            setHasMoreArticles(false);
+            return;
+        }
+
+        if(newData.length !== 0) {
+            setArticleData(prevState => [...prevState, ...newData]);
+            setPageNumber(prevState => prevState + 1);
+            return;
+        }
+
+        
+    }
+
+    useEffect(() => {
+        setIsFetching(false);
+    }, [articleData]);
+
+    const setLoading = (isFetching) ? (
+        <div className={styles.body__loading__wrapper}>
+            Fetching more articles...
+        </div>
+    ) : (
+        (hasMoreArticles) ? (
+            <button onClick={fetchArticles} className={styles.author__button__item}>Show More</button>
+        ) : (
+            null
+        )
+    );
+
     return (
         <div className={styles.body__wrapper__full}>
             <div className={styles.author__articles__wrapper}>
                 {authorCards}
                 {articleCards}
+            </div>
+            <div className={styles.author__button__wrapper}>
+                {setLoading}
             </div>
         </div>
     );
